2ee7bc2bdd7d7115204fd4af6c9e2a81627f23fc
davilasergio <davila.sergiosd@gmail.com>
2024-05-15 19:15:31 -0400

    updated GitHub hyperlinks

--- src/components/ResumeNavbar/ResumeNavbar.jsx
+++ src/components/ResumeNavbar/ResumeNavbar.jsx
@@ -6,7 +6,7 @@ const ResumeNavbar = () => {
             <p className='navbar_title'>Sergio Davila</p>
             <p>PC Build/Projects</p>
             <div className='navbar_hyperlink_container'>
-                <a className='navbar_hyperlink' href='https://github.com/davilasergio1995/' target='_blank'>Github</a>
+                <a className='navbar_hyperlink' href='https://github.com/davilasergio/' target='_blank'>Github</a>
                 <a className='navbar_hyperlink' href='https://docs.google.com/document/d/1l5_pfs336XpdTdFzg4bEKER8rTq6d2dv/edit' target='_blank'>Resume</a>
             </div>
         </div>
